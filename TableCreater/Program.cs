1586f48f9a73deaf7125df2636047aaff3d94720
Islam <denizzz9998882@gmail.com>
2019-04-21 16:52:52 +0600

    First

---
+++ TableCreater/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.SqlClient;
+
+namespace TableCreater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+               
+                string connectionString = @"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=DB1;Integrated Security=True;Pooling=False";
+
+                cn.ConnectionString = connectionString;
+                cn.Open();
+                // Работа с базой данных
+                string command = "create table gruppa(id int identity primary key, name nvarchar(40) not null )";
+                var sqlCommand = new SqlCommand(command, cn);
+                sqlCommand.ExecuteNonQuery();
+                cn.Close();
+            }
+        }
+    }
+}
